93e5f1b9682cd23b2888ecc2747115acbf12dc3f
Ruslan Garifullin <buynov0984@gmail.com>
2020-10-06 22:22:37 +0300

    Error processing and save the specified number of photos added

--- diplom.py
+++ diplom.py
@@ -8,18 +8,21 @@ class YaDisk:
         self.name = name
         self.token = token
         self.photos = photos
+        self.headers = {
+                'Accept': 'application/json',
+                'Authorization': 'OAuth ' + self.token
+                }
 
     def create_folder(self):
         URL = 'https://cloud-api.yandex.net/v1/disk/resources'
-        headers = {
-            'Accept': 'application/json',
-            'Authorization': 'OAuth ' + self.token
-        }
+
         params = {
             'path': self.name
         }
-        response = requests.put(URL, headers=headers, params=params)
-        if response.status_code == 200 or response.status_code == 201:
+        response = requests.put(URL, headers=self.headers, params=params)
+        if response.status_code not in (200, 201):
+            raise Exception('Ошибка при создании папки')
+        else:
             print('Создана папка на Яндекс Диске')
         return response
 
@@ -31,24 +34,22 @@ class YaDisk:
         }
         for photo in self.photos:
             url_get = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-            headers = {
-                'Accept': 'application/json',
-                'Authorization': 'OAuth' + ' ' + self.token
-            }
             params = {
-                'path': self.name + '/' + str(photo['likes']) + '_' + str(photo['date']) + '.png'
+                'path': self.name + '/' + str(photo['likes']) + '_' + str(photo['date']) + '.jpg'
             }
-            response_get = requests.get(url_get, headers=headers, params=params)
+            response_get = requests.get(url_get, headers=self.headers, params=params)
 
             url_put = response_get.json()['href']
             photo_get = requests.get(photo['url'])
             ph = photo_get.content
             response_put = requests.put(url_put, files={'file': ph})
-            if response_put.status_code == 200 or response_put.status_code == 201:
+            if response_put.status_code not in (200, 201):
+                raise Exception('Ошибка при загрузке файла')
+            else:
                 print('Фото загружено')
                 info['photos'].append(
                     {
-                        'file_name': str(photo['likes']) + '_' + str(photo['date']) + '.png',
+                        'file_name': self.name + '/' + str(photo['likes']) + '_' + str(photo['date']) + '.jpg',
                         'size': 'z'
                     }
                 )
@@ -60,8 +61,9 @@ class VK_user:
     photos = []
     TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
 
-    def __init__(self, id):
+    def __init__(self, id, photo_quantity):
         self.id = id
+        self.photo_quantity = photo_quantity
 
     def get_vk_photos(self):
         params = {
@@ -71,16 +73,20 @@ class VK_user:
             'rev': 0,
             'extended': 1,
             'photo_sizes': 1,
+            'count': self.photo_quantity,
             'v': 5.124
         }
 
         response = requests.get('https://api.vk.com/method/photos.get', params=params)
-        for photo in response.json()['response']['items']:
-            likes = photo['likes']['count']
-            date = photo['date']
-            url_photo = photo['sizes'][-1]['url']
-            self.photos.append({'url': url_photo, 'likes': likes, 'date': date})
-        return self.photos
+        if response.status_code not in (200, 201):
+            raise Exception('Ошибка при получении данных о фотографиях')
+        else:
+            for photo in response.json()['response']['items']:
+                likes = photo['likes']['count']
+                date = photo['date']
+                url_photo = photo['sizes'][-1]['url']
+                self.photos.append({'url': url_photo, 'likes': likes, 'date': date})
+            return self.photos
 
     def get_user_name(self):
         params = {
@@ -97,12 +103,13 @@ class VK_user:
 def user_input():
     user_id = input('Введите id пользователя Вконтакте: ')
     token_yadisk = input('Введите токен с Полигона Яндекс.Диска: ')
-    return user_id, token_yadisk
+    photo_quantity = int(input('Введите количество фотографий: '))
+    return user_id, token_yadisk, photo_quantity
 
 
 def main():
-    user_id, token_yadisk = user_input()
-    user = VK_user(user_id)
+    user_id, token_yadisk, photo_quantity = user_input()
+    user = VK_user(user_id, photo_quantity)
     photos = user.get_vk_photos()
     name = user.get_user_name()
     disk = YaDisk(name, token_yadisk, photos)
